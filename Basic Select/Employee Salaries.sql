c0c51be8c5fad869b4ced6f21fcd7917bbe85b08
Shuvajit <43761655+shuvajit-geek@users.noreply.github.com>
2023-07-26 20:54:05 +0530

    Update Employee Salaries.sql

--- Basic Select/Employee Salaries.sql
+++ Basic Select/Employee Salaries.sql
@@ -1,2 +1,2 @@
 SELECT name FROM employee WHERE salary>2000 AND months<10 ORDER BY employee_id ASC;
-/* we can use ACS or not - because by default sql short in Ascending Order.*/
+/* we didn't have to put ACS at the end - because by default sql short in Ascending Order.*/
