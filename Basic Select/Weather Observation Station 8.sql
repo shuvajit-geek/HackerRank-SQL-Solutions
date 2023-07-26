a3f0e2b63f862f2983dacc14185ea5027367c5d1
Shuvajit <43761655+shuvajit-geek@users.noreply.github.com>
2023-07-26 19:28:41 +0530

    Update Weather Observation Station 8.sql

--- Basic Select/Weather Observation Station 8.sql
+++ Basic Select/Weather Observation Station 8.sql
@@ -1,2 +1,2 @@
 /* MS SQL Server */
-SELECT city FROM STATION WHERE LOWER(city) LIKE '[aeiou]%' AND LOWER(city) LIKE '%[aeiou]';
+SELECT DISTINCT city FROM STATION WHERE LOWER(city) LIKE '[aeiou]%' AND LOWER(city) LIKE '%[aeiou]';
